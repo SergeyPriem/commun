9a298bad21b8afa55001a9fb46ae856cbef4b2ee
Main User <s.priemshiy@gmail.com>
2024-08-29 17:38:16 +0500

    refresh with checkbox

--- main.py
+++ main.py
@@ -739,7 +739,7 @@ initial_state = wf.init_state(
 
     })
 
-initial_state.import_stylesheet("theme", "/static/custom.css?155")
+initial_state.import_stylesheet("theme", "/static/custom.css?156")
 # initial_state.import_frontend_module("js_scripts", "/static/my_scripts.js?7")
 
 
